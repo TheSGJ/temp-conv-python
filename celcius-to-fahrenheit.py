6447aa49e84e2b46c1dc8bf4247cad6496091f56
TheSGJ <60005430+TheSGJ@users.noreply.github.com>
2022-04-05 15:13:33 +0530

    Update celcius-to-fahrenheit.py

--- celcius-to-fahrenheit.py
+++ celcius-to-fahrenheit.py
@@ -3,7 +3,7 @@ celsius = int(input("Enter a Celsius Temperature: "))
 
 #fuction conv to convert celcius into fahrenheit
 def conv(celsius):
-    a = 9/5 * celsius + 32 #since F = 9/5 × C + 35
+    a = 9/5 * celsius + 32 #since F = 9/5 × C + 32
     return a 
 
 fahrenheit = conv(celsius)
